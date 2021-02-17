1be7d3029a0ee611698aa134b2bd2f25ee131866
leesuncom <70786826+leesuncom@users.noreply.github.com>
2021-02-17 13:44:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,8 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '$a src-git Whitestorys https://github.com/Aibx/openwrt-packages' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git Whitestorys https://github.com/Aibx/openwrt-packages' feeds.conf.default
+git clone https://github.com/garypang13/luci-app-dnsfilter.git /package/lean/luci-app-dnsfilter

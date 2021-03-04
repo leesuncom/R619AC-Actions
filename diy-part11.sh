e3c3bf19bf4da48b078a9bd03d5a7d4bbea7b87f
leesuncom <70786826+leesuncom@users.noreply.github.com>
2021-03-04 11:46:09 +0800

    Update diy-part11.sh

--- diy-part11.sh
+++ diy-part11.sh
@@ -16,6 +16,7 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git Whitestorys https://github.com/Aibx/openwrt-packages' feeds.conf.default
+svn co https://github.com/kenzok8/openwrt-packages/tree/master/lua-maxminddb package/lua-maxminddb
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon

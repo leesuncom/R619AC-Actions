5434197519e62962f649f483aefa61bc7c456df6
leesuncom <70786826+leesuncom@users.noreply.github.com>
2021-03-07 19:36:32 +0800

    Update bypass1.sh

--- bypass1.sh
+++ bypass1.sh
@@ -24,11 +24,11 @@ git clone https://github.com/kenzok8/openwrt-packages.git
 git clone https://github.com/kenzok8/small.git
 
 # 添加bypass
-git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
+#git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
 
 # 添加bypass 依赖
 # svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns
 # svn co https://github.com/garypang13/openwrt-packages/trunk/tcping
-svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb package/lua-maxminddb
+#svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb package/lua-maxminddb
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}

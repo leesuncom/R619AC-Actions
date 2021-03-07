3cc3e89398ee2ae89a35c8d159110f217f3d7608
leesuncom <70786826+leesuncom@users.noreply.github.com>
2021-03-07 22:58:27 +0800

    Update pass1.sh

--- pass1.sh
+++ pass1.sh
@@ -14,14 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git Whitestorys https://github.com/Aibx/openwrt-packages' feeds.conf.default
-#svn co https://github.com/kenzok8/openwrt-packages/tree/master/lua-maxminddb package/lua-maxminddb
-# rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
-# git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
 
-# cd package
-# git clone https://github.com/kenzok8/openwrt-packages.git
-# git clone https://github.com/kenzok8/small.git
 # 添加dnsfilter
 git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter

57ae5ab4b5495ab0aeb3a66b4a2655ef6999d83b
leesuncom <70786826+leesuncom@users.noreply.github.com>
2021-03-10 13:04:18 +0800

    Update bypass1.sh

--- bypass1.sh
+++ bypass1.sh
@@ -14,21 +14,17 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git Whitestorys https://github.com/Aibx/openwrt-packages' feeds.conf.default
-rm -rf package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
-#git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
+sed -i '$a src-git Whitestorys https://github.com/Aibx/openwrt-packages' feeds.conf.default
 
-cd package
-git clone https://github.com/kenzok8/openwrt-packages.git
-git clone https://github.com/kenzok8/small.git
+# 添加dnsfilter
+git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-dnsfilter
 
 # 添加bypass
-#git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
+git clone https://github.com/garypang13/luci-app-bypass.git package/luci-app-bypass
 
 # 添加bypass 依赖
 # svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns
 # svn co https://github.com/garypang13/openwrt-packages/trunk/tcping
 #svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb package/lua-maxminddb
-#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
+find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
